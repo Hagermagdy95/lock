e438896b054250ccbd5a807c8edccfe5ba728cc6
Hager Magdy <Hager Magdy>
2017-01-25 04:49:08 +0200

    lock

---
+++ Lock/Lock/Lock.Shared/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lock
+{
+    public class pass
+    {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int ID { get; set; }
+        public string appname { get; set; }
+        public string accname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
